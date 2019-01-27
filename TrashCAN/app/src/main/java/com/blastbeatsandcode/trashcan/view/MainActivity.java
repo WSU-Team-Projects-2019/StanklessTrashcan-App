f1e98edb907e38efc443bd1b2d3feee128942f56
blastbeatsandcode <alexsilcott@gmail.com>
2019-01-27 10:11:33 -0500

    Added action bar and menu button to open navigation menu

--- TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/view/MainActivity.java
+++ TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/view/MainActivity.java
@@ -1,10 +1,14 @@
 package com.blastbeatsandcode.trashcan.view;
 
 import android.graphics.Color;
-import android.os.Message;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.blastbeatsandcode.trashcan.R;
@@ -14,10 +18,25 @@ import com.blastbeatsandcode.trashcan.utils.Messages;
 // MainActivity is the initial view for the application
 public class MainActivity extends AppCompatActivity implements TrashCanView {
 
+    // Reference to the drawer layout
+    private DrawerLayout _drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _drawerLayout = findViewById(R.id.drawer_layout);
+
+
+        // Set up toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+
 
         TextView tempLabel = (TextView) findViewById(R.id.lblTempLabel);
         tempLabel.setTextSize(32f);
@@ -27,6 +46,18 @@ public class MainActivity extends AppCompatActivity implements TrashCanView {
 
         Messages.makeToast(this, "Application loaded! Time to get STANKLESS!");
     }
+
+    // Open the navigation menu when the user taps on the button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                _drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public void update() {

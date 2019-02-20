6fa2720979f425e84b700bfefc7212306826aa1e
Brian Denlinger <brian.denlinger1@gmail.com>
2019-02-20 14:26:03 -0500

    Update app.py
    Add objects for lid open and lid close buttons. Implement barcode list and weight list get.

--- TrashCAN_API/app.py
+++ TrashCAN_API/app.py
@@ -1,15 +1,17 @@
+import sqlite3
+import json
 from flask import Flask, request, g
 from flask_restful import Resource, Api, reqparse
 from gpiozero import Button, OutputDevice
 from hx711 import HX711
-import sqlite3
 
 app = Flask(__name__)
 api = Api(app)
 
 #Set pins and other values
 LID_SWITCH_PIN = 23
-LID_BUTTON_PIN = 9
+LID_OPEN_PIN = 9
+LID_CLOSE_PIN = 10
 LIGHT_PIN = 24
 FAN_PIN = 25
 LED_PIN = 8
@@ -28,7 +30,8 @@ hx711 = HX711(
 
 #Create objects for physical objects
 lid_switch = Button(LID_SWITCH_PIN)
-lid_button = OutputDevice(LID_BUTTON_PIN, active_high=False, initial_value=False)
+lid_open_button = OutputDevice(LID_OPEN_PIN, active_high=False, initial_value=False)
+lid_close_button = OutputDevice(LID_CLOSE_PIN, active_high=False, initial_value=False)
 light = OutputDevice(LIGHT_PIN, active_high=False, initial_value=False)
 fan = OutputDevice(FAN_PIN, active_high=False, initial_value=False)
 led = OutputDevice(LED_PIN, active_high=False, initial_value=False)
@@ -147,47 +150,50 @@ class LightED(Resource):
 class Scale (Resource):
     def get(self):
         cur = get_db().cursor()
-        tare = cur.execute("SELECT [Value] FROM [System_Options] WHERE [Option_Name] = 'tare'")
+        cur.execute("SELECT [Value] FROM [System_Options] WHERE [Option_Name] = 'tare'")
+        tare = cur.fetchone()
         hx711.reset() #Maybe not necessary
         results = hx711.get_raw_data(NUM_MEASUREMENTS)
         return sum(results)/len(results) - tare
 
     def put(self):
+        cur = get_db().cursor()
         hx711.reset()  # Maybe not necessary
         tare = hx711.get_raw_data(NUM_MEASUREMENTS)
-        cur = get_db().cursor()
         cur.execute("UPDATE [System_Options] SET [Value] = ? WHERE [Option_Name] = 'tare'", tare)
-        return {'hello': 'world'}
+        return 'Success'
 
-class Weight (Resource):
+class WeightList (Resource):
     def get(self):
-        return 'Not implemented'
-
-    def post(self):
-        return 'Not implemented'
+        cur = get_db().cursor()
+        cur.execute("SELECT * FROM[Weight]")
+        results = cur.fetchall()
+        return json.dumps(results)
 
     def delete(self):
         return 'Not implemented'
 
-class Barcode (Resource):
+class BarcodeList (Resource):
     def get(self):
-        return 'Not implemented'
-
-    def post(self):
-        return 'Not implemented'
+        cur = get_db().cursor()
+        cur.execute("SELECT * FROM[Barcode]")
+        results = cur.fetchall()
+        return json.dumps(results)
 
     def delete(self):
         return 'Not implemented'
 
-#Map resources
+#Map resource
 api.add_resource(Index, '/')
 api.add_resource(Api, '/api')
 api.add_resource(Lid, '/api/lid')
 api.add_resource(Scale, '/api/scale')
 api.add_resource(Light, '/api/light')
 api.add_resource(Fan, '/api/fan')
-api.add_resource(Barcode, '/api/barcode')
-api.add_resource(Weight, '/api/weight')
+api.add_resource(BarcodeList, '/api/barcode')
+api.add_resource(WeightList, '/api/weight')
+api.add_resource(Barcode, '/api/barcode/<barcodeid>')
+api.add_resource(Weight, '/api/weight/<weightid>')
 
 if __name__ == '__main__':
     app.run()

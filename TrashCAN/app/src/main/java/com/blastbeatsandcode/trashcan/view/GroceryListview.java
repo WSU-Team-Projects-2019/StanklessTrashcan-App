9e62d76c13e0aadd322bd9b6f4e67aaa29801da9
blastbeatsandcode <alexsilcott@gmail.com>
2019-03-25 21:06:27 -0400

    Added initial code to test pulling down database information to the app successfully.

--- TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/view/GroceryListview.java
+++ TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/view/GroceryListview.java
@@ -3,8 +3,17 @@ package com.blastbeatsandcode.trashcan.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.blastbeatsandcode.trashcan.R;
+import com.blastbeatsandcode.trashcan.utils.Constant;
+import com.blastbeatsandcode.trashcan.utils.Messages;
 
 public class GroceryListview extends AppCompatActivity implements TrashCanView {
 
@@ -16,6 +25,22 @@ public class GroceryListview extends AppCompatActivity implements TrashCanView {
         //Enable back button
         setSupportActionBar((Toolbar) findViewById(R.id.grocery_toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, Constant.SERVER_IP + "grocerylist",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Messages.makeToast(getApplicationContext(), "Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Messages.makeToast(getApplicationContext(), "Request failed, no response received!");
+            }
+        });
+        // Add the request to the RequestQueue
+        queue.add(stringRequest);
     }
 
     @Override

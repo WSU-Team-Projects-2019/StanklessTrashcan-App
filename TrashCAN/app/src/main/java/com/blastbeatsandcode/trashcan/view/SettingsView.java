c1b769044d7f4ccb1ca25efb1327a4fde19dca34
blastbeatsandcode <alexsilcott@gmail.com>
2019-04-18 00:18:06 -0400

    cleaning cycles can now be scheduled and removed

--- TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/view/SettingsView.java
+++ TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/view/SettingsView.java
@@ -1,15 +1,21 @@
 package com.blastbeatsandcode.trashcan.view;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -21,9 +27,21 @@ import com.blastbeatsandcode.trashcan.R;
 import com.blastbeatsandcode.trashcan.utils.Constant;
 import com.blastbeatsandcode.trashcan.utils.Messages;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class SettingsView extends AppCompatActivity implements TrashCanView {
+    JSONArray jsonJobs;
     Button btnTest;
     Button btnAddJob;
     Button btnSaveSchedule;
@@ -31,6 +49,11 @@ public class SettingsView extends AppCompatActivity implements TrashCanView {
     CustomAdapter customAdapter;
 
     ArrayList<String> jobs = new ArrayList<String>();
+    int[] hours;
+    int[] mins;
+
+    Context con = this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +63,10 @@ public class SettingsView extends AppCompatActivity implements TrashCanView {
         // Get Interface features
         btnTest = (Button) findViewById(R.id.btnTestConnection);
         btnAddJob = (Button) findViewById(R.id.btnAddJob);
-        btnSaveSchedule = (Button) findViewById(R.id.btnSaveSchedule);
         jobList = (ListView) findViewById(R.id.jobList);
 
         // Create custom adapter
         customAdapter = new CustomAdapter();
-        jobList.setAdapter(customAdapter);
 
         //Enable back button
         setSupportActionBar((Toolbar) findViewById(R.id.settings_toolbar));
@@ -53,6 +74,8 @@ public class SettingsView extends AppCompatActivity implements TrashCanView {
 
         // Create volley queue
         final RequestQueue queue = Volley.newRequestQueue(this);
+
+        loadJobs();
 
         // Test the connection to the server
         btnTest.setOnClickListener(new View.OnClickListener() {
@@ -79,21 +102,169 @@ public class SettingsView extends AppCompatActivity implements TrashCanView {
         btnAddJob.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (!(jobs.size() >= 5)) {
-                    jobs.add("");
-                    jobList.setAdapter(customAdapter);
+                    // Get Current Time
+                    final Calendar c = Calendar.getInstance();
+                    int hour = c.get(Calendar.HOUR_OF_DAY);
+                    int minute = c.get(Calendar.MINUTE);
+
+                    // Launch Time Picker Dialog
+                    TimePickerDialog timePickerDialog = new TimePickerDialog(con,
+                            new TimePickerDialog.OnTimeSetListener() {
+
+                                @Override
+                                public void onTimeSet(TimePicker view, int hourOfDay,
+                                                      int minute) {
+                                    int[] tempHours = new int[hours.length + 1];
+                                    int[] tempMins = new int[mins.length + 1];
+
+                                    for (int i = 0; i < hours.length; i++)
+                                    {
+                                        tempHours[i] = hours[i];
+                                        tempMins[i] = mins[i];
+                                    }
+
+                                    // Create values for time conversion
+                                    SimpleDateFormat dateFormatUTC = new SimpleDateFormat("HH:mm");
+                                    SimpleDateFormat dateFormatLocal = new SimpleDateFormat("HH:mm");
+                                    dateFormatUTC.setTimeZone(TimeZone.getTimeZone("UTC"));
+                                    dateFormatLocal.setTimeZone(TimeZone.getDefault());
+                                    try {
+                                        // Convert from UTC to local time
+                                        Date givenDate = dateFormatLocal.parse(String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                                        String localTime = dateFormatUTC.format(givenDate);
+                                        String[] partsTime = localTime.split(":");
+
+                                        tempHours[hours.length] = Integer.parseInt(partsTime[0]);
+                                        tempMins[mins.length] = Integer.parseInt(partsTime[1]);
+
+
+                                        // Move values back over into hours and minutes
+                                        hours = new int[tempHours.length];
+                                        mins = new int[tempMins.length];
+                                        hours = Arrays.copyOf(tempHours, tempHours.length);
+                                        mins = Arrays.copyOf(tempMins, tempMins.length);
+
+
+                                        jobs.add(hours[hours.length - 1] + ":" + mins[mins.length - 1]);
+
+                                        StringRequest stringRequest = new StringRequest(Request.Method.GET, Constant.SERVER_IP + "schedule-job?hr=" + hours[hours.length - 1] + "&min=" + mins[mins.length - 1],
+                                                new Response.Listener<String>() {
+                                                    @Override
+                                                    public void onResponse(String response) {
+                                                        // Display the first 500 characters of the response string.
+                                                        Messages.makeToast(con, response);
+                                                    }
+                                                }, new Response.ErrorListener() {
+                                            @Override
+                                            public void onErrorResponse(VolleyError error) {
+                                                Messages.makeToast(con, "Request failed, no response received!");
+                                            }
+                                        });
+                                        // Add the request to the RequestQueue
+                                        queue.add(stringRequest);
+
+                                        jobList.setAdapter(customAdapter);
+                                    } catch (ParseException e) {
+                                        Messages.makeToast(getApplicationContext(), "Invalid time");
+                                    }
+                                }
+                            }, hour, minute, false);
+                    timePickerDialog.show();
                 } else {
                     Messages.makeToast(getApplicationContext(), "Maximum of 5 cleaning cycles allowed. Please remove one to insert a different time.");
                 }
             }
         });
+    }
 
-        // Save the schedule
-        btnSaveSchedule.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Messages.makeToast(getApplicationContext(), "TODO: Save the schedule");
+
+    // Pulls down scheduled jobs from the server
+    private void loadJobs()
+    {
+        // Populate the list of items from the shopping list on the server
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, Constant.SERVER_IP + "jobs",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            jsonJobs = new JSONArray(response);
+                            int len = jsonJobs.length();
+                            hours = new int[len];
+                            mins = new int[len];
+
+                            for (int i = 0; i < jsonJobs.length(); i++)
+                            {
+                                JSONObject obj = (JSONObject) jsonJobs.get(i);
+                                hours[i] = Integer.parseInt(obj.getString("hour"));
+                                mins[i] = Integer.parseInt(obj.getString("minute"));
+                                jobs.add(hours[i] + ":" + mins[i]);
+                            }
+
+                            // Create the custom adapter to fill in the food items based on our JSON array
+                            jobList = (ListView)findViewById(R.id.jobList);
+                            jobList.setAdapter(customAdapter);
+
+                            jobList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() { //list is my listView
+                                @Override
+                                public boolean onItemLongClick(AdapterView<?> arg0, View arg1, final int pos, long id) {
+                                    AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(con);
+                                    builder.setMessage("Remove clean cycle job?").setTitle("Remove scheduled clean cycle");
+
+                                    // Yes Button
+                                    builder.setPositiveButton("Yes",
+                                            new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface arg0, int arg1) {
+                                                    StringRequest stringRequest = new StringRequest(Request.Method.GET, Constant.SERVER_IP + "remove-job?hr=" + hours[pos] + "&min=" + mins[pos],
+                                                            new Response.Listener<String>() {
+                                                                @Override
+                                                                public void onResponse(String response) {
+                                                                    // Display the first 500 characters of the response string.
+                                                                    Messages.makeToast(getApplicationContext(), response);
+                                                                }
+                                                            }, new Response.ErrorListener() {
+                                                        @Override
+                                                        public void onErrorResponse(VolleyError error) {
+                                                            Messages.makeToast(getApplicationContext(), "Request failed, no response received!");
+                                                        }
+                                                    });
+                                                    // Add the request to the RequestQueue
+                                                    queue.add(stringRequest);
+
+                                                    // Reload the view
+                                                    recreate();
+                                                }
+                                            });
+
+                                    // No button
+                                    builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            // Do nothing
+                                        }
+                                    });
+
+                                    AlertDialog dialog = builder.create();
+                                    dialog.show();
+
+                                    return true;
+                                }
+                            });
+                        } catch (JSONException e) {
+                            Messages.makeToast(getApplicationContext(), "Could not properly parse data from server.");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Messages.makeToast(getApplicationContext(), "Request failed, no response received!");
             }
         });
+        // Add the request to the RequestQueue
+        queue.add(stringRequest);
     }
+
 
     @Override
     public void update() {
@@ -119,19 +290,24 @@ public class SettingsView extends AppCompatActivity implements TrashCanView {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = getLayoutInflater().inflate(R.layout.joblist_item, null);
+            // Create values for time conversion
+            SimpleDateFormat dateFormatUTC = new SimpleDateFormat("HH:mm");
+            SimpleDateFormat dateFormatLocal = new SimpleDateFormat("HH:mm");
+            dateFormatUTC.setTimeZone(TimeZone.getTimeZone("UTC"));
+            dateFormatLocal.setTimeZone(TimeZone.getDefault());
+            try {
+                // Convert from UTC to local time
+                Date givenDate = dateFormatUTC.parse(hours[position] + ":" + mins[position]);
+                String localTime = dateFormatLocal.format(givenDate);
 
-            /*
-            ImageView itemImage = (ImageView)convertView.findViewById(R.id.item_image);
-            TextView itemName = (TextView)convertView.findViewById(R.id.item_name);
-            TextView barcode = (TextView)convertView.findViewById(R.id.barcode);
-            TextView count = (TextView)convertView.findViewById(R.id.count);
+                convertView = getLayoutInflater().inflate(R.layout.joblist_item, null);
 
-            itemImage.setImageResource(R.drawable.vaporwave);
-            itemName.setText(itemNames[position]);
-            barcode.setText(barcodes[position]);
-            count.setText("Count: " + counts[position]);
-            */
+
+                TextView time = (TextView)convertView.findViewById(R.id.txtTime);
+                time.setText(localTime);
+            } catch (ParseException e) {
+                Messages.makeToast(getApplicationContext(), "Invalid time");
+            }
 
             return convertView;
         }
